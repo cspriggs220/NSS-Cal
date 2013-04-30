463cb893a7d507aaa5c2ffc8f92c699578a8958c
Cody Spriggs <codyspriggs@me.com>
2013-04-30 10:53:39 -0500

    Added methods + tests. Updated README

--- cal_integration.rb
+++ cal_integration.rb
@@ -7,14 +7,13 @@ class Cal
   # puts `cal #{month} #{year}`
 
   def initialize(month, year)
+    raise ArgumentError, "Month was not recognize. Must be between 1..12" if month <= 0 || month > 12
     @month = month
     @year = year
   end
 
-  def print_month_year(month, year)
-    # month_header
-    # days_header
-  end
+  # def print_month_year(month, year)
+  # end
 
   def find_month
     month = @month
@@ -29,23 +28,40 @@ class Cal
     header.center(20)
   end
 
-  def days_header
-    "Su Mo Tu We Th Fr Sa"
+  def days
+    days = %w{Su Mo Tu We Th Fr Sa}
   end
 
-  # def get_first_of_month(month, year) # Zeller's C
-  #   h = (q + [13(m+1)/5] + K + [K/4] + [J/4] - 2J) % 7
-  #   h = # day of the week, 0 = Sat, 1=Sunday, 2= Monday...
-  #   q = # day of the month
-  #   m = # the month 3 = March, 4 = April, 5 = May
-  #   K = # year of the century (year % 100)
-  #   J = # the actual century (year/100)
+  def days_header
+    days.join(" ")
+  end
+
+  def get_first_of_month
+    m = @month
+    y = @year
+    q = 1
+    if m < 3 # for Jan/Feb, use modified m and y values
+      y = y - 1
+      m += 12
+    end
+    h = (q + (m+1)*26/10 + y + y/4 + 6*(y/100) + y/400) % 7
+    days[h-1]
+  end
+
+  # def format_month(month, year) # line breaks
   # end
 
-  def format_month(month, year) # line breaks
-  end
-
   def leap_year?
+    year = @year
+    if year % 400 == 0
+      true
+    elsif year % 100 == 0
+      false
+    elsif year % 4 == 0
+      true
+    else
+      false
+    end
   end
 
 end
--- test/test_cal_unit.rb
+++ test/test_cal_unit.rb
@@ -9,9 +9,21 @@ class CalIntegrationTest < Test::Unit::TestCase
     assert_equal(1, cal.month)
   end
 
-  def test_09_get_year
+  def test_09a_get_year
     cal = Cal.new(1, 2013)
     assert_equal(2013, cal.year)
+  end
+
+  def test_09b_incorrect_month_argument
+    assert_raise ArgumentError do
+    cal = Cal.new(0, 2013)
+    end
+  end
+
+  def test_09c_incorrect_month_argument
+    assert_raise ArgumentError do
+    cal = Cal.new(15, 2013)
+    end
   end
 
   def test_10_get_month_header
@@ -34,4 +46,30 @@ class CalIntegrationTest < Test::Unit::TestCase
     assert_equal("     March 2003     ", cal.month_header)
     assert_equal("Su Mo Tu We Th Fr Sa", cal.days_header)
   end
+
+  def test_14_is_leap_year
+    cal = Cal.new(2, 2000)
+    assert_equal true, cal.leap_year?
+  end
+
+  def test_15_is_leap_year
+    cal = Cal.new(1, 1999)
+    assert_equal false, cal.leap_year?
+  end
+
+  def test_16_first_day_of_the_month_january
+    cal = Cal.new(1, 2013)
+    assert_equal("Tu", cal.get_first_of_month)
+  end
+
+  def test_17_first_day_of_the_month_february
+    cal = Cal.new(2, 2016)
+    assert_equal("Mo", cal.get_first_of_month)
+  end
+
+  def test_18_first_day_of_the_month_weekend
+    cal = Cal.new(10, 1803)
+    assert_equal("Sa", cal.get_first_of_month)
+  end
+
 end
