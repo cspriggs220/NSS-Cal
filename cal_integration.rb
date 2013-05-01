4a9be208d306d24ad6c447f60eac07de022c2762
Cody Spriggs <codyspriggs@me.com>
2013-04-30 23:04:18 -0500

    More Methods + Tests

--- cal_integration.rb
+++ cal_integration.rb
@@ -18,7 +18,7 @@ class Cal
   def find_month
     month = @month
     cal_array = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    get_month = cal_array[month-1]
+    cal_array[month-1]
   end
 
   def month_header
@@ -36,7 +36,7 @@ class Cal
     days.join(" ")
   end
 
-  def get_first_of_month
+  def zeller
     m = @month
     y = @year
     q = 1
@@ -45,11 +45,83 @@ class Cal
       m += 12
     end
     h = (q + (m+1)*26/10 + y + y/4 + 6*(y/100) + y/400) % 7
-    days[h-1]
+  end # returns integer for first day of month
+  # 0 = Sa; 1 = Sun; 2 = Mo; 3 = Tu; 4 = We; 5 = Th; 6 = Fr
+
+  def zeller_to_s
+    days[zeller-1]
   end
 
-  # def format_month(month, year) # line breaks
-  # end
+  def numbers_in_month
+    long_month = [1, 3, 5, 7, 8, 10, 12]
+    short_month = [4, 6, 9, 11]
+    if long_month.include?(month)
+       (1..31).to_a
+    elsif short_month.include?(month)
+       (1..30).to_a
+    elsif leap_year?
+       (1..29).to_a
+    else
+       (1..28).to_a
+    end
+  end # returns an array of the number of days in the selected month
+
+  def numbers_joined
+    numbers_in_month.join(" ")
+  end
+
+  def blank_space
+    # desired refactored code, but getting NoMethodError
+    # put zeller in order from Sun to Sat
+    # if zeller == 0
+    #   zeller = 6
+    # else
+    #   zeller - 1
+    # end
+    # "   " * zeller
+    if zeller == 1
+      ""
+    elsif zeller == 2
+      "   "
+    elsif zeller == 3
+      "      "
+    elsif zeller == 4
+      "         "
+    elsif zeller == 5
+      "            "
+    elsif zeller == 6
+      "               "
+    else zeller == 0
+      "                  "
+    end
+  end
+
+# sa (z of 0, range of 7)
+# su (z of 1, range of 6)
+# mo (z of 2, range of 5)
+# tu (z of 3, range of 4)
+# we (z of 4, range of 3)
+# th (z of 5, range of 2)
+# fr (z of 6, range of 1)
+# upper_range = 7 - z
+
+  def print_week
+    if zeller == 0
+      upper_range = 0
+      # second_week = upper_range + 1
+    else
+      upper_range = (7 - zeller)
+      # second_week = upper_range + 1
+    end
+    week = " " + blank_space + numbers_in_month[0..upper_range].join("  ") + "\n"
+    # zip enumerable?
+    # grab the next 7 days; stick it in second week
+    # grab the next 7
+    # if the number is one digit, add a blank space to the left?
+  end
+
+  def format_month(month, year) # line breaks
+  end
 
   def leap_year?
     year = @year
@@ -64,4 +136,5 @@ class Cal
     end
   end
 
+    #  Cal implemented in IRB to show newline chars "   February 2000\nSu Mo Tu We Th Fr Sa\n       1  2  3  4  5\n 6  7  8  9 10 11 12\n13 14 15 16 17 18 19\n20 21 22 23 24 25 26\n27 28 29\n\n"
 end
--- test/test_cal_unit.rb
+++ test/test_cal_unit.rb
@@ -57,19 +57,58 @@ class CalIntegrationTest < Test::Unit::TestCase
     assert_equal false, cal.leap_year?
   end
 
-  def test_16_first_day_of_the_month_january
+  def test_16a_first_day_of_the_month_january
     cal = Cal.new(1, 2013)
-    assert_equal("Tu", cal.get_first_of_month)
+    assert_equal("Tu", cal.zeller_to_s)
+  end
+
+  def test_16b_first_day_of_the_month_january
+    cal = Cal.new(1, 2013)
+    assert_equal(3, cal.zeller)
   end
 
   def test_17_first_day_of_the_month_february
     cal = Cal.new(2, 2016)
-    assert_equal("Mo", cal.get_first_of_month)
+    assert_equal("Mo", cal.zeller_to_s)
   end
 
-  def test_18_first_day_of_the_month_weekend
+  def test_18a_first_day_of_the_month_weekend
     cal = Cal.new(10, 1803)
-    assert_equal("Sa", cal.get_first_of_month)
+    assert_equal("Sa", cal.zeller_to_s)
   end
 
+  def test_18b_first_day_of_the_month_weekend
+    cal = Cal.new(10, 1803)
+    assert_equal(0, cal.zeller)
+  end
+
+  def test_19_get_correct_blank_space
+    cal = Cal.new(10, 1803)
+    assert_equal("                  ", cal.blank_space)
+  end
+
+  def test_20_get_correct_blank_space
+    cal = Cal.new(1, 2014)
+    assert_equal("         ", cal.blank_space)
+  end
+
+  def test_21_print_first_week
+    cal = Cal.new(9, 2013)
+    assert_equal(" 1  2  3  4  5  6  7\n", cal.print_week)
+  end
+
+  def test_22_print_first_week
+    cal = Cal.new(9, 2014)
+    assert_equal("    1  2  3  4  5  6\n", cal.print_week)
+  end
+
+  def test_23_print_first_week
+    cal = Cal.new(10, 1803)
+    assert_equal("                   1\n", cal.print_week)
+  end
+
+  # def test__print_first_week
+  #   cal = Cal.new(10, 1803)
+  #   assert_equal("                   1\n 2  3  4  5  6  7  8\n", cal.print_week)
+  # end
 end
